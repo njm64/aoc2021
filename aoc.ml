fa71d9a2f1e01391e5a38dc01cf2cbb02161a6e1
Nick Maher <nick.maher@gmail.com>
2022-01-02 21:11:05 +1100

    Day number can now be passed in as a command line argument

--- aoc.ml
+++ aoc.ml
@@ -1,1 +1,54 @@
-let () = Day25.run ()
+open Stdio
+open Base
+
+module type Day = sig
+  val run : unit -> unit
+end
+
+let module_for_day d =
+  match d with
+  | 1 -> Some (module Day1 : Day)
+  | 2 -> Some (module Day2 : Day)
+  | 3 -> Some (module Day3 : Day)
+  | 4 -> Some (module Day4 : Day)
+  | 5 -> Some (module Day5 : Day)
+  | 6 -> Some (module Day6 : Day)
+  | 7 -> Some (module Day7 : Day)
+  | 8 -> Some (module Day8 : Day)
+  | 9 -> Some (module Day9 : Day)
+  | 10 -> Some (module Day10 : Day)
+  | 11 -> Some (module Day11 : Day)
+  | 12 -> Some (module Day12 : Day)
+  | 13 -> Some (module Day13 : Day)
+  | 14 -> Some (module Day14 : Day)
+  | 15 -> Some (module Day15 : Day)
+  | 16 -> Some (module Day16 : Day)
+  | 17 -> Some (module Day17 : Day)
+  | 18 -> Some (module Day18 : Day)
+  | 19 -> Some (module Day19 : Day)
+  | 20 -> Some (module Day20 : Day)
+  | 21 -> Some (module Day21 : Day)
+  | 22 -> Some (module Day22 : Day)
+  | 23 -> Some (module Day23 : Day)
+  | 24 -> Some (module Day24 : Day)
+  | 25 -> Some (module Day25 : Day)
+  | _ -> None
+
+let run_day d =
+  match module_for_day d with
+  | Some m ->
+      let module M = (val m : Day) in
+      printf "Running day %d\n" d;
+      M.run ()
+  | None -> printf "No module for day %d\n" d
+
+let () =
+  let days =
+    try
+      let s = Caml.Sys.argv.(1) in
+      if String.equal s "all" then List.range 1 25 else [ Int.of_string s ]
+    with _ ->
+      printf "Usage: aoc <day_number | all>\n";
+      Caml.exit 1
+  in
+  List.iter days ~f:run_day
--- day5.ml
+++ day5.ml
@@ -7,16 +7,16 @@ let parse_cmd s =
   Caml.Scanf.sscanf s "%d,%d -> %d,%d" (fun x1 y1 x2 y2 -> (x1, y1, x2, y2))
 
 let draw_point m x y =
-  let i = y * map_size + x in
+  let i = (y * map_size) + x in
   m.(i) <- m.(i) + 1
 
 let draw_horizontal m y x1 x2 =
-  for x = (min x1 x2) to (max x1 x2) do
+  for x = min x1 x2 to max x1 x2 do
     draw_point m x y
   done
 
 let draw_vertical m x y1 y2 =
-  for y = (min y1 y2) to (max y1 y2) do
+  for y = min y1 y2 to max y1 y2 do
     draw_point m x y
   done
 
@@ -28,21 +28,18 @@ let draw_diagonal m x1 y1 x2 y2 =
   let xs = if dx < 0 then -1 else 1 in
   let ys = if dy < 0 then -1 else 1 in
   for i = 0 to count do
-    draw_point m (x1 + xs * i) (y1 + ys * i)
+    draw_point m (x1 + (xs * i)) (y1 + (ys * i))
   done
 
 let draw_line m (x1, y1, x2, y2) ~allow_diagonal =
-  if y1 = y2 then
-    draw_horizontal m y1 x1 x2 
-  else if x1 = x2 then
-    draw_vertical m x1 y1 y2
-  else if allow_diagonal then
-    draw_diagonal m x1 y1 x2 y2
+  if y1 = y2 then draw_horizontal m y1 x1 x2
+  else if x1 = x2 then draw_vertical m x1 y1 y2
+  else if allow_diagonal then draw_diagonal m x1 y1 x2 y2
 
 let print_map m =
-  Array.iteri m ~f:(fun i n -> 
-    if n > 0 then printf "%d" n else printf ".";
-    if (i + 1) % map_size = 0 then printf "\n")
+  Array.iteri m ~f:(fun i n ->
+      if n > 0 then printf "%d" n else printf ".";
+      if (i + 1) % map_size = 0 then printf "\n")
 
 let run_cmds cmds ~allow_diagonal =
   let m = Array.create ~len:(map_size * map_size) 0 in
@@ -51,7 +48,7 @@ let run_cmds cmds ~allow_diagonal =
   printf "Count: %d\n" count;
   printf "\n"
 
-let run () = 
+let run () =
   let cmds = In_channel.read_lines "input/day5.txt" |> List.map ~f:parse_cmd in
   run_cmds cmds ~allow_diagonal:false;
   run_cmds cmds ~allow_diagonal:true
